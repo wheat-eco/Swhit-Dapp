5d83c7dd7a20bbced286ceb3b917e29380960b22
wheat-eco <wheat.sol.eco@gmail.com>
2025-01-25 17:18:41 +0100

    update layout.tsx

--- app/src/app/layout.tsx
+++ app/src/app/layout.tsx
@@ -1,17 +1,15 @@
 import "./globals.css"
 import { Inter } from "next/font/google"
-import { WalletProvider } from "@mysten/dapp-kit"
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
-import { createNetworkConfig, SuiClientProvider } from "@mysten/dapp-kit"
+import { WalletKitProvider } from "@mysten/wallet-kit"
+import { SuiClientProvider } from "@mysten/sui-react"
 import { getFullnodeUrl } from "@mysten/sui.js/client"
 
 const inter = Inter({ subsets: ["latin"] })
 
-const queryClient = new QueryClient()
-const { networkConfig } = createNetworkConfig({
+const networks = {
   testnet: { url: getFullnodeUrl("testnet") },
   mainnet: { url: getFullnodeUrl("mainnet") },
-})
+}
 
 export default function RootLayout({
   children,
@@ -21,11 +19,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <QueryClientProvider client={queryClient}>
-          <SuiClientProvider networks={networkConfig} defaultNetwork="testnet">
-            <WalletProvider>{children}</WalletProvider>
-          </SuiClientProvider>
-        </QueryClientProvider>
+        <SuiClientProvider networks={networks} defaultNetwork="testnet">
+          <WalletKitProvider>{children}</WalletKitProvider>
+        </SuiClientProvider>
       </body>
     </html>
   )
